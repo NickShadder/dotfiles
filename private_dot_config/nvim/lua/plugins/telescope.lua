1fde6a6e783e3e07b6fab21a0ddd8db87ab10be7
Nick Shadder <nickshadder@users.noreply.github.com>
2024-01-08 22:05:49 +0100

    Add more telescope shortcuts

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -17,7 +17,9 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = config,
   keys = {
+    { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "[F]ind [H]elp" },
     { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "[F]ind [F]iles" },
+    { "<leader>fw", "<cmd>Telescope grep_string<CR>", desc = "[F]ind [W]ord" },
     { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "[F]ind [G]rep" },
     { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "[F]ind [B]uffers" },
   },
