207f02cba108d9e74c4ff5ae8935d213be1b665b
Nick Shadder <nickshadder@users.noreply.github.com>
2024-01-12 20:47:53 +0100

    Add more key mappings

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,42 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-local map = vim.keymap.set
 
-map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- Map command silently
+local maps = function(mode, key, command)
+  vim.keymap.set(mode, key, command, { silent = true })
+end
+
+-- Unmap space because it is the <leader> key
+maps({ 'n', 'v' }, '<Space>', '<nop>')
 
 -- Indentation
-map("v", "<Tab>", ">gv", { silent = true })
-map("v", "<S-Tab>", "<gv", { silent = true })
+maps("v", "<Tab>", ">gv")
+maps("v", "<S-Tab>", "<gv")
 
+-- Move highlighted lines while adjusting their indentation
+maps("v", "J", ":m '>+1<CR>gv=gv")
+maps("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in place when joining lines
+maps("n", "J", "mzJ`z")
+
+-- Keep search terms in the middle
+maps("n", "n", "nzzzv")
+maps("n", "N", "Nzzzv")
+
+-- Preserve copied text when pasting
+maps("x", "<leader>p", [["_dP]])
+
+-- Yank and delete into the system clipboard
+maps("n", "<leader>Y", [["+Y]])
+maps({ "n", "v" }, "<leader>y", [["+y]])
+maps({ "n", "v" }, "<leader>d", [["+d]])
+
+-- Unbind Q
+maps("n", "Q", "<nop>")
+
+-- Replace word under cursor
+maps("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Autoformat
+vim.keymap.set("n", "C-A-l", vim.lsp.buf.format)
