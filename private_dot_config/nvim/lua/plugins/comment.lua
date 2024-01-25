4376a500bb29113be9682c673286823fe6eed9a1
Nick Shadder <nickshadder@users.noreply.github.com>
2024-01-25 22:06:05 +0100

    No longer move the cursor when commenting

--- private_dot_config/nvim/lua/plugins/comment.lua
+++ private_dot_config/nvim/lua/plugins/comment.lua
@@ -1,14 +1,6 @@
 return {
   "numToStr/Comment.nvim",
-  opts = {
-    -- Move the cursor down one line after (un)commenting
-    -- if we only touched one line. Like in IntelliJ
-    post_hook = function(ctx)
-      if ctx.range.srow == ctx.range.erow then
-        vim.cmd("norm! j")
-      end
-    end,
-  },
+  config = true,
   keys = {
     { "<C-_>", "<ESC>gcc", mode = { "n", "i" }, remap = true },
     { "<C-\\>", "<ESC>gbc", mode = { "n", "i" }, remap = true },
