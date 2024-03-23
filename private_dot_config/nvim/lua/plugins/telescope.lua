ea93b09dbfb002038fc0f355f14642ecca853c7f
Nick Shadder <nickshadder@users.noreply.github.com>
2024-03-23 17:17:22 +0100

    Add conditional telescope dependency on fzf-native

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,15 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      -- `cond` is a condition used to determine whether this plugin should be
+      -- installed and loaded.
+      cond = function()
+        return vim.fn.executable "make" == 1
+      end,
+    },
     { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
   },
   opts = {
