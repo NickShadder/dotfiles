c49c559b1973e1c902013f8d99daa15a6f0d97d4
Nick Shadder <nickshadder@users.noreply.github.com>
2024-03-27 00:10:47 +0100

    Add lsp configuration

--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -84,6 +84,7 @@ local config = function()
     },
 
     sources = cmp.config.sources {
+      { name = 'nvim_lsp' },
       { name = "luasnip" },
       { name = "buffer" },
       { name = "path" },
@@ -120,6 +121,7 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind-nvim",
