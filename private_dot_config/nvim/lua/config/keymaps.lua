91200acbb8250c626d4d272cc0ceb38075fa9b0a
Nick Shadder <nickshadder@users.noreply.github.com>
2024-03-27 00:06:00 +0100

    Add diagnostic navigation keymaps

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -52,3 +52,9 @@ maps("n", "<leader>|", "<cmd>vsplit<cr>")
 
 -- Autoformat
 vim.keymap.set("n", "C-A-l", vim.lsp.buf.format)
+
+-- Diagnostics
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
