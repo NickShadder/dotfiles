3822e0ce758928bd5178aaa2f4882a3e3a99a52b
Nick Shadder <nickshadder@users.noreply.github.com>
2024-03-30 00:20:03 +0100

    Fix dependency tree for luasnip

--- private_dot_config/nvim/lua/plugins/luasnip.lua
+++ private_dot_config/nvim/lua/plugins/luasnip.lua
@@ -10,10 +10,4 @@ return {
     end
     return 'make install_jsregexp'
   end)(),
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
 }
--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,6 +13,8 @@ local config = function()
   local devicons = require("nvim-web-devicons")
   local autopairs = require("nvim-autopairs.completion.cmp")
 
+  require("luasnip.loaders.from_vscode").lazy_load()
+
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -123,10 +125,15 @@ return {
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind-nvim",
     "windwp/nvim-autopairs",
+    {
+      "rafamadriz/friendly-snippets",
+      dependencies = {
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+      }
+    },
   },
   config = config,
 }
