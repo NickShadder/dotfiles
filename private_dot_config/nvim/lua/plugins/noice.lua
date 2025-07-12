a70ed571f75e68a271646f8aaca1d2fc21e857e9
Nick Shadder <nickshadder@users.noreply.github.com>
2025-07-12 15:52:10 +0200

    Extend noice configuration

--- private_dot_config/nvim/lua/plugins/noice.lua
+++ private_dot_config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,56 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
-    -- add any options here
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          any = {
+            { find = "%d+L, %d+B" },
+            { find = "; after #%d+" },
+            { find = "; before #%d+" },
+          },
+        },
+        view = "mini",
+      },
+    },
+    presets = {
+      command_palette = true,
+    },
   },
-  dependencies = { "MunifTanjim/nui.nvim" },
+  keys = {
+    { "<leader>nlm", function()
+      require("noice").cmd("last")
+    end, desc = "[N]oice [L]ast [M]essage" },
+    { "<leader>nh", function()
+      require("noice").cmd("history")
+    end, desc = "[N]oice [H]istory" },
+    { "<leader>na", function()
+      require("noice").cmd("all")
+    end, desc = "[N]oice [A]ll" },
+    { "<leader>nd", function()
+      require("noice").cmd("dismiss")
+    end, desc = "[N]oice [D]ismiss All" },
+    { "<c-f>", function()
+      if not require("noice.lsp").scroll(4) then
+        return "<c-f>"
+      end
+    end, silent = true, expr = true, desc = "Scroll forward", mode = { "i", "n", "s" } },
+    { "<c-b>", function()
+      if not require("noice.lsp").scroll(-4) then
+        return "<c-b>"
+      end
+    end, silent = true, expr = true, desc = "Scroll backward", mode = { "i", "n", "s" } },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
 }
