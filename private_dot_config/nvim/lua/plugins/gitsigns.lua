d8360d7fc747e6b32779a0e0135e9107bdc46bd9
Nick Shadder <nickshadder@users.noreply.github.com>
2025-07-13 17:28:07 +0200

    Use opts instead of config for gitsigns

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -4,22 +4,17 @@ return {
     signs = {
       add = { text = "+" },
     },
-  },
-  config = function()
-    require("gitsigns").setup {
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map("n", "<leader>gsh", gs.preview_hunk)
-        map("n", "<leader>gtd", gs.toggle_deleted)
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
       end
-    }
-  end,
 
+      map("n", "<leader>gsh", gs.preview_hunk)
+      map("n", "<leader>gtd", gs.toggle_deleted)
+    end
+  },
 }
