24cb300bc73491f2fb36caedb1ae1a4cc3416dc5
Nick Shadder <nickshadder@users.noreply.github.com>
2025-07-20 16:45:53 +0200

    Switch treesitter to opts

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,49 +3,48 @@ return {
   branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "java",
-        "json",
-        "kotlin",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "regex",
-        "rust",
-        "vim",
-        "vimdoc",
-        "yaml",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "java",
+      "json",
+      "kotlin",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "rust",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    auto_install = true,
+    indent = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        node_incremental = "v",
+        node_decremental = "V",
       },
-      auto_install = true,
-      indent = {
-        enable = true,
+    },
+    lsp_interop = {
+      enable = true,
+      border = "none",
+      floating_preview_opts = {},
+      peek_definition_code = {
+        ["<leader>df"] = "@function.outer",
+        ["<leader>dF"] = "@class.outer",
       },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_incremental = "v",
-          node_decremental = "V",
-        },
-      },
-      lsp_interop = {
-        enable = true,
-        border = "none",
-        floating_preview_opts = {},
-        peek_definition_code = {
-          ["<leader>df"] = "@function.outer",
-          ["<leader>dF"] = "@class.outer",
-        },
-      },
-    }
-  end,
+    },
+  },
 }
